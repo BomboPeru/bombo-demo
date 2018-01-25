c64270ab830c794e2f42698f000e646fb92d0b7e
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2018-01-25 02:55:20 -0500

    Created basic interaction and flags system

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 
 import PlayerSelector from './components/player_selector';
 import glamorous from 'glamorous';
+import { StyleSheet, css } from 'aphrodite';
 
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
 
 const Stadium = glamorous.div({
     backgroundColor: '#25BF89',
     padding: '1.2em',
-    textAlign: 'center'
+    textAlign: 'center',
+    minHeight: '300px'
+});
+const StadiumAction = glamorous.div({
+    backgroundColor: '#25BFB9',
+    padding: '1.2em',
+    textAlign: 'center',
+    border: 0
 });
 
 const Title = glamorous.h1({
@@ -16,9 +24,62 @@ const Title = glamorous.h1({
     padding: '1.2em'
 });
 
+const marginBtn = StyleSheet.create({
+    left: {
+        marginLeft: '5px'
+    },
+    right: {
+        marginRight: '5px'
+    }
+});
+
 class App extends Component {
     constructor(props) {
         super(props);
+        this.props = props;
+        this.addPLayer = this.addPlayer.bind(this);
+        this.subPlayer = this.subPlayer.bind(this);
+
+        this.maxTotalPlayers = 11;
+
+        this.bounds = {
+            porteros: [1, 1],
+            defensas: [3, 5],
+            centrocampistas: [3, 5],
+            delanteros: [3, 5]
+        };
+
+        this.state = {
+            totalPlayers: 0,
+            porteros: [],
+            defensas: [],
+            centrocampistas: [],
+            delanteros: []
+        };
+    }
+
+    addPlayer(type, i) {
+        console.log(i);
+        if (
+            this.state[type].length < this.bounds[type][1] &&
+            this.state.totalPlayers < this.maxTotalPlayers
+        ) {
+            this.setState(prevState => {
+                prevState.totalPlayers = prevState.totalPlayers + 1;
+                prevState[type].push(<PlayerSelector kind={type} key={i} />);
+                return prevState;
+            });
+        }
+    }
+
+    subPlayer(type) {
+        if (this.state[type].length > this.bounds[type][0]) {
+            this.setState(prevState => {
+                prevState.totalPlayers = prevState.totalPlayers - 1;
+                prevState[type].pop();
+                return prevState;
+            });
+        }
     }
 
     render() {
@@ -28,37 +89,127 @@ class App extends Component {
                 <Stadium>
                     <Row type="flex" justify="space-around" align="center">
                         <Col span={5}>
-                            <h2>
-                                Arquero
-                            </h2>
-                            <PlayerSelector kind={'porteros'} />
+                            <h2>Arquero</h2>
+                            {this.state.porteros.map((sel, i) => {
+                                return sel;
+                            })}
                         </Col>
                         <Col span={5}>
                             <h2>Defensas</h2>
-                            <PlayerSelector kind={'defensas'} />
-                            <PlayerSelector kind={'defensas'} />
-                            <PlayerSelector kind={'defensas'} />
-                            <PlayerSelector kind={'defensas'} />
-                            <PlayerSelector kind={'defensas'} />
+                            {this.state.defensas.map((sel, i) => {
+                                return sel;
+                            })}
                         </Col>
                         <Col span={5}>
-                            <h2>Volantes</h2>
-                            <PlayerSelector kind={'centrocampistas'} />
-                            <PlayerSelector kind={'centrocampistas'} />
-                            <PlayerSelector kind={'centrocampistas'} />
-                            <PlayerSelector kind={'centrocampistas'} />
-                            <PlayerSelector kind={'centrocampistas'} />
+                            <h2>Centrocampistas</h2>
+                            {this.state.centrocampistas.map((sel, i) => {
+                                return sel;
+                            })}
                         </Col>
                         <Col span={5}>
                             <h2>Delanteros</h2>
-                            <PlayerSelector kind={'delanteros'} />
-                            <PlayerSelector kind={'delanteros'} />
-                            <PlayerSelector kind={'delanteros'} />
-                            <PlayerSelector kind={'delanteros'} />
-                            <PlayerSelector kind={'delanteros'} />
+                            {this.state.delanteros.map((sel, i) => {
+                                return sel;
+                            })}
                         </Col>
                     </Row>
                 </Stadium>
+                <StadiumAction>
+                    <Row type="flex" justify="space-around" align="center">
+                        <Col span={5}>
+                            <Button
+                                icon="plus"
+                                className={css(marginBtn.right)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.addPlayer(
+                                        'porteros',
+                                        this.state.totalPlayers
+                                    );
+                                }}>
+                                Add Player
+                            </Button>
+                            <Button
+                                icon="minus"
+                                className={css(marginBtn.left)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.subPlayer('porteros');
+                                }}>
+                                Sub player
+                            </Button>
+                        </Col>
+                        <Col span={5}>
+                            <Button
+                                icon="plus"
+                                className={css(marginBtn.right)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.addPlayer(
+                                        'defensas',
+                                        this.state.totalPlayers
+                                    );
+                                }}>
+                                Add Player
+                            </Button>
+                            <Button
+                                icon="minus"
+                                className={css(marginBtn.left)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.subPlayer('defensas');
+                                }}>
+                                Sub player
+                            </Button>
+                        </Col>
+                        <Col span={5}>
+                            <Button
+                                icon="plus"
+                                className={css(marginBtn.right)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.addPlayer(
+                                        'centrocampistas',
+                                        this.state.totalPlayers
+                                    );
+                                }}>
+                                Add Player
+                            </Button>
+                            <Button
+                                icon="minus"
+                                className={css(marginBtn.left)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.subPlayer('centrocampistas');
+                                }}>
+                                Sub player
+                            </Button>
+                        </Col>
+                        <Col span={5}>
+                            <Button
+                                icon="plus"
+                                className={css(marginBtn.right)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.addPlayer(
+                                        'delanteros',
+                                        this.state.totalPlayers
+                                    );
+                                }}>
+                                Add Player
+                            </Button>
+                            <Button
+                                icon="minus"
+                                className={css(marginBtn.left)}
+                                onClick={e => {
+                                    e.preventDefault();
+                                    this.subPlayer('delanteros');
+                                }}>
+                                Sub player
+                            </Button>
+                        </Col>
+                    </Row>
+                </StadiumAction>
             </div>
         );
     }
--- src/components/player_selector.js
+++ src/components/player_selector.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { Select } from 'antd';
+import { Select, Icon } from 'antd';
 import axios from 'axios';
+import blankGif from '../assets/blank.gif';
 
 class PlayerSelector extends React.Component {
     constructor(props) {
         super(props);
         this.kindOfPlayer = props.kind;
+        this.onPlayerChange = props.onPlayerChange;
+
         this.listFetch = this.listFetch.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.playersOptions = [];
         this.state = { loading: true, player: null };
-
-
     }
     componentDidMount() {
-      this.listFetch();
-
+        this.listFetch();
     }
     listFetch() {
-        this.setState(() => {
-            return { loading: true };
+        this.setState(prevState => {
+            prevState.loading = true;
+            return prevState;
         });
         axios.get('http://localhost:9800/api/v1/full').then(resp => {
             const fdata = resp.data;
@@ -27,38 +28,59 @@ class PlayerSelector extends React.Component {
                 const mPlayers = fdata[team][this.kindOfPlayer];
                 this.playersOptions = this.playersOptions.concat(mPlayers);
             }
-            console.log(this.playersOptions);
-            this.setState(() => {
-                return { loading: false };
+            // console.log(this.playersOptions);
+            this.setState(prevState => {
+                prevState.loading = false;
+                return prevState;
             });
         });
     }
     handleChange(value) {
-        this.setState((prevState) => {
-            return {...prevState, player: this.playersOptions[value]}
+        this.setState(prevState => {
+            return { ...prevState, player: this.playersOptions[value] };
         });
         console.log(`selected ${value}`);
+        if (this.onPlayerChange) {
+            this.onPlayerChange(this.state.player);
+        }
     }
     render() {
         return (
             <div>
+                {this.state.loading ? (
+                    <Icon type="loading" style={{ marginRight: '8px' }} />
+                ) : null}
+
                 <Select
                     style={{ width: 220 }}
                     onChange={this.handleChange}
-                    showSearch
-                >
+                    showSearch>
                     {this.playersOptions.map((pl, i) => {
                         return (
-                            <Select.Option
-                                value={i}
-                                key={i}
-                            >
-                                {pl.nation} - {pl.name}
+                            <Select.Option value={i} key={i}>
+                                {pl.team[0].toUpperCase() +
+                                    pl.team.substring(1)}{' '}
+                                - {pl.name}
                             </Select.Option>
-                        )
+                        );
                     })}
                 </Select>
-                <h2>{this.state.player?this.state.player.team.toString():''}</h2>
+                <h2>
+                    {this.state.player ? (
+                        (
+                            <div>
+                                <img
+                                    src={blankGif}
+                                    class={`flag flag-${this.state.player.nation_code}`}
+                                    style={{display: 'inline-bloc'}}
+                                />
+                                <h3 style={{display: 'inline-bloc'}}> {this.state.player.nation} </h3>
+                            </div>
+                        )
+                    ) : (
+                        ''
+                    )}
+                </h2>
             </div>
         );
     }
